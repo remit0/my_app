1b1f1878b12f73c4a3e406d018e5ca973d51617d
Remi Rosenthal <remi-rosenthal@hotmail.fr>
2025-07-09 18:33:24 +0200

    test

--- backend/fetch_api.py
+++ backend/fetch_api.py
@@ -5,4 +5,4 @@ fetch_api = Blueprint("fetch_api", __name__, url_prefix="/api")
 
 @fetch_api.route("/hello", methods=["GET"])
 def hello():
-    return jsonify({"key": "hello"})
+    return jsonify({"key": "hellooo"})
